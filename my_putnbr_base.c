4e79020965fd9d526b3fe48db048884bb66b4ec6
Otyne <43828915+otyne@users.noreply.github.com>
2018-10-09 19:03:55 +0200

    Add files via upload

--- my_putnbr_base.c
+++ my_putnbr_base.c
@@ -7,17 +7,48 @@
 
 #include "my.h"
 
-int my_putnbr_base(int nbr, char const *base)
+static int displaynbr_base(int nbr, int base_len, char const *base)
 {
-    int base_len = 0;
     int i = 0;
-	
-    while (base[base_len] != '\0')
-        base_len += 1;
-    i = nbr % base_len;
-    if (nbr != 0) {
-        my_putnbr_base(nbr / base_len, base);
+    
+    if (nbr < 0) {
+        my_putchar('-');
+        i = nbr % base_len * -1;
+        displaynbr_base(nbr / base_len * -1, base_len, base);
+        my_putchar(base[i]);
+    }
+    if (nbr > 0) {
+        i = nbr % base_len;
+        displaynbr_base(nbr / base_len, base_len, base);
         my_putchar(base[i]);
     }
     return (0);
 }
+
+int my_putnbr_base(int nbr, char const *base)
+{
+    int base_len = 0;
+
+    while (base[base_len] != '\0')
+        base_len += 1;
+    if (nbr == 0) {
+        my_putchar('0');
+        return (0);
+    }
+    return (displaynbr_base(nbr, base_len, base));
+}
+
+/* int my_putnbr_base(int nbr, char const *base) */
+/* { */
+/*     int base_len = 0; */
+/*     int i = 0; */
+
+/*     while (base[base_len] != '\0') */
+/*         base_len += 1; */
+/*     i = nbr % base_len; */
+/*     if (nbr != 0) { */
+/*         my_putnbr_base(nbr / base_len, base); */
+/*         my_putchar(base[i]); */
+/*     } */
+/*     return (0); */
+/* } */
